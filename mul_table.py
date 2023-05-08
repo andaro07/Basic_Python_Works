e83d17e7f6c442389553f36cafd181dcdb176ea6
Ronald Anim <animdarkoronald081@gmail.com>
2023-05-08 15:29:57 +0000

    Basic multiplication table in python using for loop

---
+++ mul_table.py
@@ -0,0 +1,5 @@
+print("\n******MULTIPLICATION TABLE*********")
+for first_num in range(1,13):
+    print(f"\n    {first_num}\n")
+    for second_num in range(1,13):
+        print(f"{first_num} * {second_num} = {first_num * second_num}")
